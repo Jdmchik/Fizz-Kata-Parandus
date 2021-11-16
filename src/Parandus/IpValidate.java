61f6a13b0973f155bb677544b15e8536ce5d5fda
ilja filatov <opilane@HP-CZC6297B8J.tthk.ee>
2021-11-16 16:30:57 +0200

    Add correct solution test 2

--- src/Parandus/IpValidate.java
+++ src/Parandus/IpValidate.java
@@ -4,13 +4,16 @@ public class IpValidate {
 
 
 	public Boolean ValidateIpv4Address(String ipString) {
-		if(ipString=="") {
+		if(hasThreeDots(ipString)) {
 			return true;
 		}
 		return false;
 	}
-	
-}
 
+	private boolean hasThreeDots(String ipString) {
+		return ipString.chars().filter(c->c=='.').count()==3;
+	}
+
+}
 
 
